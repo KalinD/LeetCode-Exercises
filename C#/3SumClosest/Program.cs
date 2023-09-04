f7d89797ef8258a853065e82e679ecd47b1de02c
Kalin Doychev <doiychevk@gmail.com>
2023-09-04 17:18:06 +0200

    Solved 3Sum Closest

---
+++ C#/3SumClosest/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _3SumClosest;
+
+public class Solution
+{
+    public int ThreeSumClosest(int[] nums, int target)
+    {
+        int start = 0;
+        int closest = 0;
+        bool isInitial = true;
+        Array.Sort(nums);
+        while (start < nums.Length - 2)
+        {
+            int left = start + 1;
+            int right = nums.Length - 1;
+            if (isInitial)
+            {
+                closest = nums[start] + nums[left] + nums[right];
+                isInitial = false;
+            }
+            if (nums[start] == 60) {
+                Console.WriteLine(nums[start]);
+            }
+            while (left < right)
+            {
+                int s = nums[start] + nums[left] + nums[right];
+                Console.WriteLine(s);
+                if (Math.Abs(target - s) < Math.Abs(target - closest))
+                {
+                    closest = s;
+                }
+                if(target - s < 0)
+                {
+                    right--;
+                } else if (target - s > 0) { 
+                    left++;
+                }
+                else { 
+                    return target;
+                }
+            }
+            start++;
+        }
+        return closest;
+    }
+
+    public static void Main(string[] args) { }
+}
